826aee35f7a45ae469758a88f63be2cea4f72328
felipe buongermino <felipe.buongermino@gmail.com>
2022-03-10 17:02:04 -0300

    TaxiFareModel com ML flow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -9,6 +9,10 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from TaxiFareModel.data import get_data, clean_data
 from sklearn.model_selection import train_test_split
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
+from ml_flow_test import EXPERIMENT_NAME
 
 
 class Trainer():
@@ -20,6 +24,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = EXPERIMENT_NAME
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -55,6 +60,27 @@ class Trainer():
         print(rmse)
         return rmse
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri("https://mlflow.lewagon.co/")
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 if __name__ == "__main__":
     # store the data in a DataFrame
@@ -85,3 +111,7 @@ if __name__ == "__main__":
     # evaluate the pipeline
     print('evaluation do pipeline')
     rmse = trainer.evaluate(X_test, y_test)
+
+
+    experiment_id = trainer.mlflow_experiment_id
+    print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
